be6bc30ad70e58b9b6fc8748aeed90d4658e92af
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-07 21:39:55 +0000

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# cree un archivo en /tmp
+
+file { '/tmp/holberton':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+this is CM configuration management
