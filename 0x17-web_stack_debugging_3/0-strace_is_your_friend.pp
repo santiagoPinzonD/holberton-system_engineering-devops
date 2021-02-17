5cf91e6bab11d9b9ea02ea5f6b2c40cc20723bdd
santiagopinzonD <santiagopinzon456@gmail.com>
2021-02-17 21:50:22 +0000

    fix puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Corrects phpp extension for php
 exec {'phppFix':
     path    => '/bin',
-        command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
